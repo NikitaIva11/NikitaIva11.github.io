aabb36b3309e8961f346dbfe9dc655eb207da1a5
NikitaIva11 <98526129+NikitaIva11@users.noreply.github.com>
2022-02-10 19:12:40 +0300

    Update lection_4.js

--- Hilel HW/lectionJS/lection_4.js
+++ Hilel HW/lectionJS/lection_4.js
@@ -1,21 +1,21 @@
 let a = []
 let n = 10
 
-for (let index = 0; index < n; index++) {
+for (let index = 0;index < n;index++) {
     a[index] = []
     for (let j = 0; j < n; j++) {
         let randomNum = Math.round((Math.random() * 40) - 20)
         a[index][j] = randomNum
     }
 }
-for (let index = 0; index < n; index++) {
+for (let index = 0;index < n;index++) {
     let sum = 0;
-    for (let j = 0; j < n; j++) {
+    for (let j = 0;j < n;j++) {
         sum += a[j][index]
     }
     if (sum < 0) {
         console.log(index, sum)
-        for (let z = 0; z < n; z++) {
+        for (let z = 0;z < n;z++) {
             delete a[z][index]
         }
     }
