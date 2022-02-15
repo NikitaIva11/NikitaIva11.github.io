3a2beb167b04f8cfb8a1d9a3b2497c71f3f9f2d0
nikblood <73354171+nikblood@users.noreply.github.com>
2022-02-16 01:08:31 +0200

    commit

---
+++ Hilel HW/lectionJS/lection_5.js
@@ -0,0 +1,19 @@
+const a = [60,'qwdqwdqw',70,'wqdqwdqw',80,90,100];
+const b = ['wqdqwdq',10,'qwdqwdqw',20,30,40,50];
+
+function sum(a,b){
+    let sumA = 0;
+    let sumB = 0;
+    for(let i = 0;i<a.length;i++){
+        if(typeof a[i] === 'number'){
+            sumA+=a[i];
+        }
+        if(typeof b[i] === 'number'){
+            sumB+=b[i];
+        }
+    }
+    let findArr = sumA>sumB?a:b;
+    return findArr
+}
+let bigArr = sum(a,b);
+console.log(bigArr);
