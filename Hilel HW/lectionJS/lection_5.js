e4e6f83cc117ab38aacc208bbaa9f3e32862a2fb
NikitaIva11 <98526129+NikitaIva11@users.noreply.github.com>
2022-02-16 02:16:28 +0300

    Update lection_5.js

--- Hilel HW/lectionJS/lection_5.js
+++ Hilel HW/lectionJS/lection_5.js
@@ -1,9 +1,10 @@
 const a = [60,'qwdqwdqw',70,'wqdqwdqw',80,90,100];
 const b = ['wqdqwdq',10,'qwdqwdqw',20,30,40,50];
+const n = a.length;
 function sum(a,b){
     let sumA = 0;
     let sumB = 0;
-    for(let i = 0;i<a.length;i++){
+    for(let i = 0;i<n;i++){
         if(typeof a[i] === 'number'&&a[i]!=NaN){
             sumA+=a[i];
         }
