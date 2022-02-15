d7c25838b3272b096fd682a709d25cdf127b3ef7
nikblood <73354171+nikblood@users.noreply.github.com>
2022-02-15 22:24:06 +0200

    commit

---
+++ Hilel HW/lectionJS/lection_5.js
@@ -0,0 +1,19 @@
+const list = [0,0,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1,0,1]
+const n = list.length
+
+function compress(list){
+    const arr = [];
+    let step = 0;
+    for(let i = 0;i<n;i++){
+        let current = list[i];
+        let next = list[i+1];
+        if(current!==next){
+            arr.push((i+1)-step);
+            step = i+1;
+            continue;
+        }
+    }
+    return arr;
+}
+let newList = compress(list);
+console.log(newList)
