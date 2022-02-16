96f018fd93a11b0ff59d233ff5521635fc1cf36f
nikblood <73354171+nikblood@users.noreply.github.com>
2022-02-16 13:33:48 +0200

    commit

--- Hilel HW/lectionJS/lection_6.js
+++ Hilel HW/lectionJS/lection_6.js
@@ -1,23 +1,22 @@
 const arr = [1, 2, 3, 4, 5, 6];
 const n = arr.length;
-
 function copy(arr, func) {
     let newArr = [];
-        for (let i = 0; i < n; i++) {
-            newArr.push(arr[i]);
-            func!=undefined?
-            console.log(func(newArr[i]))
-            :
-            console.log(undefined);
-        }
+    if (typeof func === 'function') {
+        for (let i = 0; i < n; i++) { newArr.push(func(arr[i])); }
+    } else {
+        for (let i = 0; i < n; i++) { newArr.push(arr[i]); }
+    }
     return newArr;
     //--------------------------------
     // Решение через map
-    // let newArr = arr.map(el => {
-    //     func(el)
-    //     return el
-    // })
+    // if (typeof func === 'function') {
+    //     newArr = arr.map(el => { return func(el) })
+    // } else {
+    //     newArr = arr.map(el => { return el })
+    // }
+    // return newArr
 }
-let newArr = copy(arr, function (value) { return value * 10 })
-console.log(newArr)
-
+let newArr = copy(arr, function (value) { return value * 10 });
+console.log(newArr);
+console.log(arr);
