380deff808f95cfd4944a29f4de430483b55dddc
nikblood <73354171+nikblood@users.noreply.github.com>
2022-07-12 10:23:47 +0200

    commit

--- Hilel HW/lection24_1/client/client_server/client_server.js
+++
@@ -1,9 +0,0 @@
-let http = require('http');
-let static = require('node-static');
-let file = new static.Server('../client_side');
-let port = 7274;
-
-http.createServer(function(req,res){
-     file.serve(req,res)
-}).listen(port)
-
